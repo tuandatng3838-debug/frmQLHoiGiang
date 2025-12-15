1974afc7d0453c41fbaab4900ca131aea271acf5
Mai Thành <150693168+Thanhuoa@users.noreply.github.com>
2025-12-15 08:25:03 +0700

    Refactor FrmMain layout and button configurations

--- src/FrmQLHoiGiang/Forms/FrmMain.Designer.cs
+++ src/FrmQLHoiGiang/Forms/FrmMain.Designer.cs
@@ -31,11 +31,11 @@ partial class FrmMain
         components = new System.ComponentModel.Container();
         siticoneBorderlessForm1 = new Siticone.Desktop.UI.WinForms.SiticoneBorderlessForm(components);
         panelMenu = new Siticone.Desktop.UI.WinForms.SiticonePanel();
+        btnGiangVien = new Siticone.Desktop.UI.WinForms.SiticoneButton();
+        btnSangKien = new Siticone.Desktop.UI.WinForms.SiticoneButton();
         btnThongKe = new Siticone.Desktop.UI.WinForms.SiticoneButton();
         btnLichGiang = new Siticone.Desktop.UI.WinForms.SiticoneButton();
-        btnSangKien = new Siticone.Desktop.UI.WinForms.SiticoneButton();
         btnHoiGiang = new Siticone.Desktop.UI.WinForms.SiticoneButton();
-        btnGiangVien = new Siticone.Desktop.UI.WinForms.SiticoneButton();
         panelHeader = new Siticone.Desktop.UI.WinForms.SiticonePanel();
         flowHeaderTools = new FlowLayoutPanel();
         controlBoxClose = new Siticone.Desktop.UI.WinForms.SiticoneControlBox();
@@ -45,42 +45,80 @@ partial class FrmMain
         panelContainer = new Siticone.Desktop.UI.WinForms.SiticonePanel();
         panelMenu.SuspendLayout();
         panelHeader.SuspendLayout();
+        flowHeaderTools.SuspendLayout();
         SuspendLayout();
         // 
         // siticoneBorderlessForm1
         // 
         siticoneBorderlessForm1.ContainerControl = this;
         siticoneBorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+        siticoneBorderlessForm1.DragEndTransparencyValue = 0.9D;
+        siticoneBorderlessForm1.ResizeForm = false;
         siticoneBorderlessForm1.TransparentWhileDrag = true;
         // 
         // panelMenu
         // 
         panelMenu.BackColor = Color.White;
+        panelMenu.Controls.Add(btnGiangVien);
+        panelMenu.Controls.Add(btnSangKien);
         panelMenu.Controls.Add(btnThongKe);
         panelMenu.Controls.Add(btnLichGiang);
-        panelMenu.Controls.Add(btnSangKien);
         panelMenu.Controls.Add(btnHoiGiang);
-        panelMenu.Controls.Add(btnGiangVien);
         panelMenu.Dock = DockStyle.Left;
-        panelMenu.FillColor = Color.FromArgb(245, 247, 250);
+        panelMenu.FillColor = Color.White;
         panelMenu.Location = new Point(0, 0);
-        panelMenu.Margin = new Padding(4, 5, 4, 5);
+        panelMenu.Margin = new Padding(3, 4, 3, 4);
         panelMenu.Name = "panelMenu";
-        panelMenu.Padding = new Padding(23, 53, 23, 27);
-        panelMenu.Size = new Size(314, 1067);
+        panelMenu.Padding = new Padding(18, 42, 18, 22);
+        panelMenu.Size = new Size(265, 872);
         panelMenu.TabIndex = 0;
+        panelMenu.Paint += panelMenu_Paint;
+        // 
+        // btnGiangVien
+        // 
+        btnGiangVien.BackColor = SystemColors.MenuHighlight;
+        btnGiangVien.ButtonMode = Siticone.Desktop.UI.WinForms.Enums.ButtonMode.RadioButton;
+        btnGiangVien.Checked = true;
+        btnGiangVien.CustomBorderColor = Color.Turquoise;
+        btnGiangVien.FillColor = Color.White;
+        btnGiangVien.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold);
+        btnGiangVien.ForeColor = Color.FromArgb(64, 64, 64);
+        btnGiangVien.Location = new Point(0, 113);
+        btnGiangVien.Margin = new Padding(3, 4, 3, 4);
+        btnGiangVien.Name = "btnGiangVien";
+        btnGiangVien.Size = new Size(265, 140);
+        btnGiangVien.TabIndex = 0;
+        btnGiangVien.Tag = "GV";
+        btnGiangVien.Text = "Cán bộ - giảng viên";
+        btnGiangVien.Click += btnGiangVien_Click;
+        // 
+        // btnSangKien
+        // 
+        btnSangKien.BackColor = SystemColors.MenuHighlight;
+        btnSangKien.ButtonMode = Siticone.Desktop.UI.WinForms.Enums.ButtonMode.RadioButton;
+        btnSangKien.FillColor = Color.White;
+        btnSangKien.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold);
+        btnSangKien.ForeColor = Color.FromArgb(64, 64, 64);
+        btnSangKien.Location = new Point(0, 389);
+        btnSangKien.Margin = new Padding(3, 4, 3, 4);
+        btnSangKien.Name = "btnSangKien";
+        btnSangKien.Size = new Size(265, 140);
+        btnSangKien.TabIndex = 2;
+        btnSangKien.Tag = "SK";
+        btnSangKien.Text = "Sáng kiến - cải tiến";
+        btnSangKien.Click += btnSangKien_Click;
         // 
         // btnThongKe
         // 
-        btnThongKe.BorderRadius = 8;
+        btnThongKe.BackColor = SystemColors.MenuHighlight;
         btnThongKe.ButtonMode = Siticone.Desktop.UI.WinForms.Enums.ButtonMode.RadioButton;
         btnThongKe.FillColor = Color.White;
-        btnThongKe.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+        btnThongKe.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold);
         btnThongKe.ForeColor = Color.FromArgb(64, 64, 64);
-        btnThongKe.Location = new Point(23, 580);
-        btnThongKe.Margin = new Padding(4, 5, 4, 5);
+        btnThongKe.Location = new Point(0, 664);
+        btnThongKe.Margin = new Padding(3, 4, 3, 4);
         btnThongKe.Name = "btnThongKe";
-        btnThongKe.Size = new Size(269, 75);
+        btnThongKe.Size = new Size(265, 140);
         btnThongKe.TabIndex = 4;
         btnThongKe.Tag = "TK";
         btnThongKe.Text = "Thống kê";
@@ -88,85 +126,35 @@ partial class FrmMain
         // 
         // btnLichGiang
         // 
-        btnLichGiang.BorderRadius = 8;
+        btnLichGiang.BackColor = SystemColors.MenuHighlight;
         btnLichGiang.ButtonMode = Siticone.Desktop.UI.WinForms.Enums.ButtonMode.RadioButton;
         btnLichGiang.FillColor = Color.White;
-        btnLichGiang.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+        btnLichGiang.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold);
         btnLichGiang.ForeColor = Color.FromArgb(64, 64, 64);
-        btnLichGiang.Location = new Point(23, 475);
-        btnLichGiang.Margin = new Padding(4, 5, 4, 5);
+        btnLichGiang.Location = new Point(0, 527);
+        btnLichGiang.Margin = new Padding(3, 4, 3, 4);
         btnLichGiang.Name = "btnLichGiang";
-        btnLichGiang.Size = new Size(269, 75);
+        btnLichGiang.Size = new Size(265, 140);
         btnLichGiang.TabIndex = 3;
         btnLichGiang.Tag = "LG";
         btnLichGiang.Text = "Lịch giảng dạy";
         btnLichGiang.Click += btnLichGiang_Click;
         // 
-        // btnSangKien
-        // 
-        btnSangKien.BorderRadius = 8;
-        btnSangKien.ButtonMode = Siticone.Desktop.UI.WinForms.Enums.ButtonMode.RadioButton;
-        btnSangKien.FillColor = Color.White;
-        btnSangKien.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
-        btnSangKien.ForeColor = Color.FromArgb(64, 64, 64);
-        btnSangKien.Location = new Point(23, 370);
-        btnSangKien.Margin = new Padding(4, 5, 4, 5);
-        btnSangKien.Name = "btnSangKien";
-        btnSangKien.Size = new Size(269, 75);
-        btnSangKien.TabIndex = 2;
-        btnSangKien.Tag = "SK";
-        btnSangKien.Text = "Sáng kiến - cải tiến";
-        btnSangKien.Click += btnSangKien_Click;
-        // 
         // btnHoiGiang
         // 
-        btnHoiGiang.BorderRadius = 8;
+        btnHoiGiang.BackColor = SystemColors.MenuHighlight;
         btnHoiGiang.ButtonMode = Siticone.Desktop.UI.WinForms.Enums.ButtonMode.RadioButton;
         btnHoiGiang.FillColor = Color.White;
-        btnHoiGiang.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+        btnHoiGiang.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold);
         btnHoiGiang.ForeColor = Color.FromArgb(64, 64, 64);
-        btnHoiGiang.Location = new Point(23, 265);
-        btnHoiGiang.Margin = new Padding(4, 5, 4, 5);
+        btnHoiGiang.Location = new Point(0, 253);
+        btnHoiGiang.Margin = new Padding(3, 4, 3, 4);
         btnHoiGiang.Name = "btnHoiGiang";
-        btnHoiGiang.Size = new Size(269, 75);
+        btnHoiGiang.Size = new Size(265, 140);
         btnHoiGiang.TabIndex = 1;
         btnHoiGiang.Tag = "HG";
         btnHoiGiang.Text = "Hội giảng";
         btnHoiGiang.Click += btnHoiGiang_Click;
-        // 
-        // btnGiangVien
-        // 
-        btnGiangVien.BorderRadius = 8;
-        btnGiangVien.ButtonMode = Siticone.Desktop.UI.WinForms.Enums.ButtonMode.RadioButton;
-        btnGiangVien.Checked = true;
-        btnGiangVien.FillColor = Color.White;
-        btnGiangVien.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
-        btnGiangVien.ForeColor = Color.FromArgb(64, 64, 64);
-        btnGiangVien.Location = new Point(23, 160);
-        btnGiangVien.Margin = new Padding(4, 5, 4, 5);
-        btnGiangVien.Name = "btnGiangVien";
-        btnGiangVien.Size = new Size(269, 75);
-        btnGiangVien.TabIndex = 0;
-        btnGiangVien.Tag = "GV";
-        btnGiangVien.Text = "Cán bộ - giảng viên";
-        btnGiangVien.Click += btnGiangVien_Click;
-        // 
-        // flowHeaderTools
-        // 
-        flowHeaderTools.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-        flowHeaderTools.AutoSize = true;
-        flowHeaderTools.BackColor = Color.Transparent;
-        flowHeaderTools.FlowDirection = FlowDirection.RightToLeft;
-        flowHeaderTools.Location = new Point(940, 33);
-        flowHeaderTools.Margin = new Padding(0);
-        flowHeaderTools.Name = "flowHeaderTools";
-        flowHeaderTools.Padding = new Padding(0);
-        flowHeaderTools.Size = new Size(0, 0);
-        flowHeaderTools.TabIndex = 4;
-        flowHeaderTools.WrapContents = false;
-        flowHeaderTools.Controls.Add(controlBoxClose);
-        flowHeaderTools.Controls.Add(controlBoxMinimize);
-        flowHeaderTools.Controls.Add(lblUser);
         // 
         // panelHeader
         // 
@@ -174,33 +162,55 @@ partial class FrmMain
         panelHeader.Controls.Add(label1);
         panelHeader.Dock = DockStyle.Top;
         panelHeader.FillColor = Color.White;
-        panelHeader.Location = new Point(314, 0);
-        panelHeader.Margin = new Padding(4, 5, 4, 5);
+        panelHeader.Location = new Point(265, 0);
+        panelHeader.Margin = new Padding(3, 4, 3, 4);
         panelHeader.Name = "panelHeader";
-        panelHeader.Padding = new Padding(34, 20, 34, 20);
-        panelHeader.Size = new Size(1400, 131);
+        panelHeader.Padding = new Padding(27, 16, 27, 16);
+        panelHeader.Size = new Size(1106, 105);
         panelHeader.TabIndex = 1;
+        // 
+        // flowHeaderTools
+        // 
+        flowHeaderTools.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+        flowHeaderTools.AutoSize = true;
+        flowHeaderTools.BackColor = Color.Transparent;
+        flowHeaderTools.Controls.Add(controlBoxClose);
+        flowHeaderTools.Controls.Add(controlBoxMinimize);
+        flowHeaderTools.Controls.Add(lblUser);
+        flowHeaderTools.FlowDirection = FlowDirection.RightToLeft;
+        flowHeaderTools.Location = new Point(892, 27);
+        flowHeaderTools.Margin = new Padding(0);
+        flowHeaderTools.Name = "flowHeaderTools";
+        flowHeaderTools.Size = new Size(187, 53);
+        flowHeaderTools.TabIndex = 4;
+        flowHeaderTools.WrapContents = false;
         // 
         // controlBoxClose
         // 
+        controlBoxClose.Anchor = AnchorStyles.Top | AnchorStyles.Right;
         controlBoxClose.BorderRadius = 4;
         controlBoxClose.FillColor = Color.White;
         controlBoxClose.HoverState.FillColor = Color.FromArgb(240, 85, 68);
         controlBoxClose.HoverState.IconColor = Color.White;
         controlBoxClose.IconColor = Color.FromArgb(64, 64, 64);
+        controlBoxClose.Location = new Point(134, 2);
+        controlBoxClose.Margin = new Padding(2);
         controlBoxClose.Name = "controlBoxClose";
-        controlBoxClose.Size = new Size(64, 60);
+        controlBoxClose.Size = new Size(51, 48);
         controlBoxClose.TabIndex = 2;
         // 
         // controlBoxMinimize
         // 
+        controlBoxMinimize.Anchor = AnchorStyles.Top | AnchorStyles.Right;
         controlBoxMinimize.BorderRadius = 4;
         controlBoxMinimize.ControlBoxType = Siticone.Desktop.UI.WinForms.Enums.ControlBoxType.MinimizeBox;
         controlBoxMinimize.FillColor = Color.White;
         controlBoxMinimize.HoverState.FillColor = Color.FromArgb(236, 242, 255);
-        controlBoxMinimize.IconColor = Color.FromArgb(64, 64, 64);
+        controlBoxMinimize.IconColor = Color.Black;
+        controlBoxMinimize.Location = new Point(79, 2);
+        controlBoxMinimize.Margin = new Padding(2);
         controlBoxMinimize.Name = "controlBoxMinimize";
-        controlBoxMinimize.Size = new Size(64, 60);
+        controlBoxMinimize.Size = new Size(51, 48);
         controlBoxMinimize.TabIndex = 3;
         // 
         // lblUser
@@ -208,10 +218,10 @@ partial class FrmMain
         lblUser.AutoSize = true;
         lblUser.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
         lblUser.ForeColor = Color.FromArgb(31, 122, 224);
-        lblUser.Location = new Point(0, 20);
-        lblUser.Margin = new Padding(0, 20, 16, 0);
+        lblUser.Location = new Point(0, 16);
+        lblUser.Margin = new Padding(0, 16, 13, 0);
         lblUser.Name = "lblUser";
-        lblUser.Size = new Size(70, 28);
+        lblUser.Size = new Size(64, 23);
         lblUser.TabIndex = 1;
         lblUser.Text = "Admin";
         lblUser.TextAlign = ContentAlignment.MiddleRight;
@@ -221,40 +231,43 @@ partial class FrmMain
         label1.AutoSize = true;
         label1.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold);
         label1.ForeColor = Color.FromArgb(31, 122, 224);
-        label1.Location = new Point(69, 60);
-        label1.Margin = new Padding(4, 0, 4, 0);
+        label1.Location = new Point(55, 48);
         label1.Name = "label1";
-        label1.Size = new Size(247, 38);
+        label1.Size = new Size(211, 32);
         label1.TabIndex = 0;
         label1.Text = "Quản lý Hội giảng";
         // 
         // panelContainer
         // 
-        panelContainer.Dock = DockStyle.Fill;
+        panelContainer.Dock = DockStyle.Top;
         panelContainer.FillColor = Color.White;
-        panelContainer.Location = new Point(314, 131);
-        panelContainer.Margin = new Padding(4, 5, 4, 5);
+        panelContainer.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 163);
+        panelContainer.Location = new Point(265, 105);
+        panelContainer.Margin = new Padding(3, 4, 3, 4);
         panelContainer.Name = "panelContainer";
-        panelContainer.Size = new Size(1400, 936);
+        panelContainer.Size = new Size(1106, 781);
         panelContainer.TabIndex = 2;
+        panelContainer.Paint += panelContainer_Paint;
         // 
         // FrmMain
         // 
-        AutoScaleDimensions = new SizeF(10F, 25F);
+        AutoScaleDimensions = new SizeF(8F, 20F);
         AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(1714, 1067);
+        ClientSize = new Size(1371, 872);
         Controls.Add(panelContainer);
         Controls.Add(panelHeader);
         Controls.Add(panelMenu);
         FormBorderStyle = FormBorderStyle.None;
-        Margin = new Padding(4, 5, 4, 5);
-        MinimumSize = new Size(1714, 1067);
+        Margin = new Padding(3, 4, 3, 4);
+        MinimumSize = new Size(1371, 814);
         Name = "FrmMain";
         StartPosition = FormStartPosition.CenterScreen;
         Text = "FrmMain";
         panelMenu.ResumeLayout(false);
         panelHeader.ResumeLayout(false);
         panelHeader.PerformLayout();
+        flowHeaderTools.ResumeLayout(false);
+        flowHeaderTools.PerformLayout();
         ResumeLayout(false);
     }
 
